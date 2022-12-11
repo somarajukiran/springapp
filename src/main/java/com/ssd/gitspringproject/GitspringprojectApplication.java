0d350093f3f68ce1725b4820bdf024ebfeb28a7e
somarajukiran <kirangithub28@gmail.com>
2022-12-11 10:31:30 +0530

    project template

---
+++ src/main/java/com/ssd/gitspringproject/GitspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.gitspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitspringprojectApplication.class, args);
+	}
+
+}
