9eaa6c14d0895fb4753983d0a7c8f6b8c5efc709
somarajukiran <kirangithub28@gmail.com>
2022-12-11 18:02:03 +0530

    added controller

--- src/main/java/com/ssd/gitspringproject/entity/Employee.java
+++ src/main/java/com/ssd/gitspringproject/entity/Employee.java
@@ -4,13 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "emp_table")
 public class Employee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
 	private String name;
 	private String address;
 	private Long phNo;
